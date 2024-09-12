f11e6d811426b56db420c7e43a0648c51b7b7546
vkumar-forbes <vkumar@forbesadvisor.com>
2024-09-12 14:35:05 +0530

    remove unused code

--- solid-reac/src/assets/forbes-advisor/index.tsx
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite-plugin-svgr/client" />
-
-export { default as WhiteLogo } from "./white-logo.svg?react";
-export { default as SidebarLogo } from "./sidebar-logo.svg?react";
-export { default as FooterLogo } from "./footer-logo.svg?react";
--- solid-reac/src/assets/icons/index.tsx
+++
@@ -1,8 +0,0 @@
-export { default as ArrowUpIcon } from "./arrow-up.svg?react";
-export { default as ChevronDownIcon } from "./chevron-down.svg?react";
-export { default as FooterCopyIcon } from "./footer-copy-icon.svg?react";
-export { default as FooterLockIcon } from "./footer-lock-icon.svg?react";
-export { default as PrimaryLockIcon } from "./primary-lock-icon.svg?react";
-export { default as SidebarStar } from "./sidebar-star.svg?react";
-export { default as SidebarAward } from "./sidebar-award.svg?react";
-export { default as SidebarThumbsUp } from "./sidebar-thumbs-up.svg?react";
